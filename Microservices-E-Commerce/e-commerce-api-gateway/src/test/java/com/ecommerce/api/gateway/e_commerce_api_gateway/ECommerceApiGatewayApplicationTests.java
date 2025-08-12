6d69bcd5462c3f40c130d3b5839f8bbf6b103c2c
luisesales <duduosales@hotmail.com>
2025-08-12 20:39:14 -0300

    first commit

---
+++ Microservices-E-Commerce/e-commerce-api-gateway/src/test/java/com/ecommerce/api/gateway/e_commerce_api_gateway/ECommerceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.api.gateway.e_commerce_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Microservices-E-Commerce/e-commerce-config-server/src/test/java/bankaiconfigserver/bankai_config_server/BankaiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bankaiconfigserver.bankai_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
