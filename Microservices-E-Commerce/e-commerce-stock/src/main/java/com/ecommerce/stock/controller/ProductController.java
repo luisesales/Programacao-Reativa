87feb5cd3350a459c063a6eec17273455f617bf5
luisesales <duduosales@hotmail.com>
2025-10-23 22:25:31 -0300

    removing cache for creation of model rest-mvc-with no redis with bugs on mcp requests

--- Microservices-E-Commerce/e-commerce-stock/src/main/java/com/ecommerce/stock/controller/ProductController.java
+++ Microservices-E-Commerce/e-commerce-stock/src/main/java/com/ecommerce/stock/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.ecommerce.stock.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.redisson.api.RedissonReactiveClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +25,6 @@ import com.ecommerce.stock.service.ProductService;
 public class ProductController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
-
-    @Autowired
-    private RedissonReactiveClient redissonReactiveClient;
 
     @Autowired
     private ProductService productService;
