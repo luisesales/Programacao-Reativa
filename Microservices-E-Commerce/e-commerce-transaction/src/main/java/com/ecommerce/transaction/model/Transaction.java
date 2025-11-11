baaed5446eb3b378159112c9553db0d349f78bb5
luisesales <duduosales@hotmail.com>
2025-11-11 14:34:59 -0300

    more changes in transactions

---
+++ Microservices-E-Commerce/e-commerce-transaction/src/main/java/com/ecommerce/transaction/model/Transaction.java
@@ -0,0 +1,57 @@
+package com.ecommerce.transaction.model;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Table(name = "transactions")
+public class Transaction {
+      @Id 
+    private UUID id;
+    @NotBlank(message = "Name is mandatory")
+    @Size(min = 5, message = "The name must have at least 5 characters")
+    private String name;
+    @Transient
+    private UUID orderId;
+
+    @NotNull(message = "Total Price is mandatory")
+    @Column("total_price")
+    private double totalPrice;   
+    
+    public Transaction() {
+        this.id = null;
+        this.name = "N/A";
+        this.orderId = null;        
+        this.totalPrice = 0.0;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public UUID getOrderId() {
+        return orderId;
+    }
+    public void setOrderId(UUID orderId) {
+        this.orderId = orderId;
+    }
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
