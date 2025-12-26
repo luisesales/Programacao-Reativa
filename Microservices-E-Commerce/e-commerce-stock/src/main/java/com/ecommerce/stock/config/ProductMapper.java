807bf2dba9acc339cba99c56cb20ad839153d86b
luisesales <duduosales@hotmail.com>
2025-12-26 18:26:42 -0300

    starting docker update for message version

--- Microservices-E-Commerce/e-commerce-stock/src/main/java/com/ecommerce/stock/config/ProductMapper.java
+++ Microservices-E-Commerce/e-commerce-stock/src/main/java/com/ecommerce/stock/config/ProductMapper.java
@@ -1,10 +1,18 @@
 package com.ecommerce.stock.config;
 
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
 import com.ecommerce.stock.model.Product;
 import com.ecommerce.stock.model.dto.ProductInputDTO;
-import org.mapstruct.*;
 
+@Configuration
 @Mapper(componentModel = "spring")
+@Component
 public interface ProductMapper {
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
